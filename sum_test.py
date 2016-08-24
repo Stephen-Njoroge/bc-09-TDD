4aa513f2f0e8a74a91e9e4e99a2795f2cae3910b
Stephen-Njoroge <steven.n360@gmail.com>
2016-08-24 04:11:03 +0300

    Adding five test cases.

--- sum_test.py
+++ sum_test.py
@@ -18,6 +18,16 @@ class MySumTests(unittest.TestCase):
         self.assertEqual(my_sum(19,6), 25)
         self.assertEqual(my_sum(91,6), 97)
         self.assertEqual(my_sum(9,6), 15)
+    def test_sum_of_numbers(self):
+        '''
+        Test sum of digits/numbers
+        '''
+        self.assertNotEqual(my_sum(10, 18), 25)
+        self.assertNotEqual(my_sum(9, 6), 65)
+        self.assertNotEqual(my_sum(9, 9), 78)
+        self.assertNotEqual(my_sum(19,6), 265)
+        self.assertNotEqual(my_sum(91,6), 7)
+        self.assertNotEqual(my_sum(9,6), 165)
 
     def test_non_numbers(self):
         '''
@@ -30,6 +40,14 @@ class MySumTests(unittest.TestCase):
         	my_sum(7, '9')
         with self.assertRaises(TypeError):
         	 my_sum(5, 'b')
+    def my_sum(self):
+        self.my_sum = my_sum()
+
+    def test_if_both_args_not_numbers(self):
+        with self.assertRaises(TypeError):
+            my_sum('two', 'three')
+
+ 
 
 if __name__ == '__main__':
     unittest.main()
